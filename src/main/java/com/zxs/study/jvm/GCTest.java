64c1c95563e15c02a254262a683ddbdfbb85a149
mrszhang <mrszhang1996@163.com>
2022-07-04 11:32:08 +0800

    add new file

--- src/main/java/com/zxs/study/jvm/GCTest.java
+++ src/main/java/com/zxs/study/jvm/GCTest.java
@@ -1,25 +1,27 @@
 package com.zxs.study.jvm;
 
+import java.util.Properties;
+
 /**
  * gc测试 增加-XX:+PrintGCDetails
- *  大对象就是需要大量连续内存空间的对象（比如：字符串、数组）。JVM参数 -XX:PretenureSizeThreshold 可以设置大对象的大小，如果对象超过设置大小会直接进入老年代，不会进入年轻代，这个参数只在 Serial 和ParNew两个收集器下有效。
+ * 大对象就是需要大量连续内存空间的对象（比如：字符串、数组）。JVM参数 -XX:PretenureSizeThreshold 可以设置大对象的大小，如果对象超过设置大小会直接进入老年代，不会进入年轻代，这个参数只在 Serial 和ParNew两个收集器下有效。
  * 比如设置JVM参数：-XX:PretenureSizeThreshold=1000000 (单位是字节)  -XX:+UseSerialGC  ，再执行下上面的第一个程序会发现大对象直接进了老年代
  * 为什么要这样呢？
  * 为了避免为大对象分配内存时的复制操作而降低效率。
- *
+ * <p>
  * 对象晋升到老年代的年龄阈值，可以通过参数 -XX:MaxTenuringThreshold 来设置。
+ *
  * @author s1mp1e
  * @date 1/7/2022
  */
 public class GCTest {
 
     public static void main(String[] args) {
-        byte[] object60M = new byte[50 * 1024 * 1024];
-
-        byte[] object10M = new byte[10 * 1024 * 1024];
-
-        byte[] object5M = new byte[5 * 1024 * 1024];
-
+        Properties properties = System.getProperties();
+        for (String key : properties.stringPropertyNames()) {
+            //输出对应的键和值
+            System.out.println(key + "=" + properties.getProperty(key));
+        }
     }
 
 }
---
+++ src/main/java/com/zxs/study/leetcode/Question2.java
@@ -0,0 +1,139 @@
+package com.zxs.study.leetcode;
+
+import java.util.List;
+
+/**
+ * 给你两个非空 的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0开头。
+ * 链接：https://leetcode.cn/problems/add-two-numbers
+ *
+ * @author s1mp1e
+ * @date 1/7/2022
+ */
+public class Question2 {
+
+
+    public static void main(String[] args) {
+
+        Question2 question2 = new Question2();
+
+        int[] nums1 = new int[]{9, 9, 9, 9, 9, 9, 9};
+        ListNode l1 = question2.createListNode(nums1);
+        int[] nums2 = new int[]{9, 9, 9, 9};
+        ListNode l2 = question2.createListNode(nums2);
+
+        ListNode result = question2.addTwoNumbers2(l1, l2);
+        System.out.println(result);
+
+
+    }
+
+    private static final ThreadLocal<Boolean> local = new ThreadLocal<Boolean>() {
+        @Override
+        protected Boolean initialValue() {
+            return false;
+        }
+    };
+
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int sum = val1 + val2 + carry;
+            ListNode node = new ListNode(sum % 10);
+            if (head == null) {
+                head = tail = node;
+            } else {
+                tail.next = node;
+                tail = node;
+            }
+            if (sum > 10) {
+                carry = 1;
+            }
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        if (carry == 1) {
+            tail.next = new ListNode(1);
+        }
+        return head;
+    }
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int sum;
+        boolean canReturn = false;
+        ListNode result = new ListNode();
+        if (l1 == null && l2 == null) {
+            sum = 0;
+            canReturn = true;
+        } else if (l1 == null && l2 != null) {
+            sum = l2.val;
+        } else if (l1 != null && l2 == null) {
+            sum = l1.val;
+        } else {
+            sum = l1.val + l2.val;
+        }
+        if (local.get()) {
+            sum += 1;
+        }
+        if (sum >= 10) {
+            result.val = sum % 10;
+            local.set(true);
+        } else {
+            result.val = sum;
+            local.set(false);
+        }
+        if (canReturn && sum == 0) {
+            return null;
+        } else {
+            result.next = addTwoNumbers(l1 == null ? null : l1.next, l2 == null ? null : l2.next);
+            return result;
+        }
+    }
+
+
+    private ListNode createListNode(int[] nums) {
+        ListNode listNode = new ListNode(nums[0]);
+        ListNode current = listNode;
+        for (int i = 1; i < nums.length; i++) {
+            ListNode next = new ListNode(nums[i]);
+            current.next = next;
+            current = next;
+        }
+        return listNode;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(val);
+            ListNode nextNode = next;
+            while (nextNode != null) {
+                stringBuilder.append(nextNode.val);
+                nextNode = nextNode.next;
+            }
+            return stringBuilder.toString();
+        }
+    }
+}
